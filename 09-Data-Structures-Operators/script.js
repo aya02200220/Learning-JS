d150174322765417ba1ea1e82b0e151aa652f252
Aya <pyon220@gmail.com>
2022-12-27 04:10:11 -0800

    dec 27th

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
 const openingHours = {
@@ -21,59 +17,59 @@ const openingHours = {
   },
 };
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  // ES6 enhanced object literals
-  openingHours,
+//   // ES6 enhanced object literals
+//   openingHours,
 
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
+//   order(starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
 
-  oderDelivery(address = 1, mainIndex = 1, starterIndex = 1, time = 1) {
-    console.log(
-      `Order received! ${this.mainMenu[mainIndex]} and ${this.starterMenu[starterIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
+//   oderDelivery(address = 1, mainIndex = 1, starterIndex = 1, time = 1) {
+//     console.log(
+//       `Order received! ${this.mainMenu[mainIndex]} and ${this.starterMenu[starterIndex]} will be delivered to ${address} at ${time}`
+//     );
+//   },
 
-  orderPasta(ing1, ing2, ing3) {
-    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
-  },
+//   orderPasta(ing1, ing2, ing3) {
+//     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+//   },
 
-  orderPizza(mainIngredient, ...otherIngredient) {
-    console.log(mainIngredient);
-    console.log(otherIngredient);
-  },
-};
+//   orderPizza(mainIngredient, ...otherIngredient) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredient);
+//   },
+// };
 
-if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+// if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
 
-// With optional chaining
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);
+// // With optional chaining
+// console.log(restaurant.openingHours.mon?.open);
+// console.log(restaurant.openingHours?.mon?.open);
 
-// Example
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`on ${day}, we open at ${open}`);
-}
+// // Example
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? 'closed';
+//   console.log(`on ${day}, we open at ${open}`);
+// }
 
-// Methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+// // Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
 
-// Arrays
-const users = [{ name: 'jonas', email: 'hello@jonas.io' }];
-console.log(users[0]?.name ?? 'Users array empty');
+// // Arrays
+// const users = [{ name: 'jonas', email: 'hello@jonas.io' }];
+// console.log(users[0]?.name ?? 'Users array empty');
 
-if (users.length > 0) console.log(users[0].name);
-else console.log('User array empty');
+// if (users.length > 0) console.log(users[0].name);
+// else console.log('User array empty');
 
 ////////////////////////////////////////////////////////
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
@@ -202,3 +198,244 @@ console.log(i, j, k);
 // Default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);*/
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// for (const [num, name] of game.scored.entries())
+//   console.log(`Goal:${num + 1} by ${name}`);
+
+// // 2.
+// const ave = Object.values(game.odds);
+// console.log(ave);
+// let i = 0;
+// for (const calcOdds of ave) i += calcOdds;
+// i /= ave.length;
+// console.log(i);
+
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['Correct', 3],
+//   [true, 'You are correct🎉'],
+//   [false, 'You are wrong💀'],
+// ]);
+
+// // const answer = Number(prompt('Your answer'));
+// const answer = 3;
+// console.log(question.get(answer === question.get('Correct')));
+
+// console.log(question);
+// console.log([...question]);
+// console.log(question.entries());
+
+/*
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
+
+// const events = [...new Set(gameEvents.values())];
+// // const events = new Set(gameEvents);
+// console.log(events);
+
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
+
+// const time = [...gameEvents.keys()].pop();
+// console.log(
+//   `An event happened, on average, every ${time / gameEvents.size} minutes`
+// );
+
+// for (const [key, value] of gameEvents) {
+//   const half = key <= 45 ? 'FIRST' : 'SECOND';
+//   console.log(`[${half} HALF] ${key}:${value}`);
+// }
+
+// const airline = 'TAP Air Japan';
+// const plane = 'A320';
+
+// const checkSeat = function (seat) {
+//   if (seat.slice(-1) === 'B' || seat.slice(-1) === 'C') {
+//     console.log('You got the middle seat!');
+//   } else {
+//     console.log('You got lucky❤️');
+//   }
+// };
+
+// checkSeat('12B');
+// checkSeat('123C');
+// checkSeat('12A');
+
+// const capitalizeName = function (name) {
+//   const lowerName = name.toLowerCase();
+//   const names = lowerName.split(' ');
+//   const nArray = [];
+
+//   for (const n of names) {
+//     nArray.push(n[0].toUpperCase() + n.slice(1));
+//   }
+//   console.log(nArray.join(' '));
+// };
+
+// capitalizeName('aya ishimura');
+// capitalizeName('miharu sasaki');
+// capitalizeName('matthew aaron weightman');
+
+// const maskCreditCard = function (number) {
+//   const num = number + '';
+//   const lastFourNum = num.slice(-4);
+//   return lastFourNum.padStart(number.length, '*');
+//   // const coveredNum = '*'.repeat(num.length - lastFourNum.length) + lastFourNum;
+//   // console.log(coveredNum);
+// };
+// console.log(maskCreditCard('d989283409524234'));
+// console.log(maskCreditCard('dscdcsdcfvevrvbr'));
+// console.log(maskCreditCard('d9892834023325435952423ss4'));
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
+//   let i = 0;
+//   for (const row of rows) {
+//     i += 1;
+//     const [output1, output2] = row.trim().toLowerCase().split('_');
+//     // const output3 = output2[0].toUpperCase() + output2.slice(1);
+//     // arr.push([output1, output3].join(''));
+//     let result = output1 + output2[0].toUpperCase() + output2.slice(1);
+//     console.log(result.padEnd(20, ' ') + '♥️'.repeat(i));
+//   }
+// });
+
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
+//   for (const [i, row] of rows.entries()) {
+//     const [output1, output2] = row.trim().toLowerCase().split('_');
+//     // const output3 = output2[0].toUpperCase() + output2.slice(1);
+//     // arr.push([output1, output3].join(''));
+//     let result = output1 + output2[0].toUpperCase() + output2.slice(1);
+//     console.log(result.padEnd(20, ' ') + '♥️'.repeat(i + 1));
+//   }
+// });
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const createCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  console.log(
+    `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+      '_',
+      ' '
+    )} from ${createCode(from)} to ${createCode(to)} (${time.replace(
+      ':',
+      'h'
+    )})`.padStart(45)
+  );
+}
